5b7f02ce057d1dce0286e949f80a258757e5f0a7
Kanazawa Shion <iroha1521@gmail.com>
2021-03-15 11:11:21 +0900

    Playerの移動だけ実装した

--- GameSources/Player.cpp
+++ GameSources/Player.cpp
@@ -7,6 +7,64 @@
 #include "Project.h"
 
 namespace basecross{
+	void Player::OnCreate()
+	{
+		auto drawComp = AddComponent<BcPNTStaticDraw>();
+		drawComp->SetMeshResource(L"DEFAULT_CUBE");
+	}
+
+	void Player::SetSpeed()
+	{
+		auto& app = App::GetApp();
+		float ElapsedTime = app->GetElapsedTime();
+		auto cntlPad = app->GetInputDevice().GetControlerVec();//コントローラーの取得
+
+		float fThumbLY = 0.0f;
+		float fThumbLX = 0.0f;
+		if (cntlPad[0].bConnected)
+		{
+			fThumbLY = cntlPad[0].fThumbLY;
+			fThumbLX = cntlPad[0].fThumbLX;
+		}
+
+		if (fThumbLX != 0 || fThumbLY != 0)
+		{
+			//コントローラの向き
+			float moveX = fThumbLX;
+			float moveZ = fThumbLY;
+			Vec2 moveVec(moveZ, moveX);
+			float moveSize = moveVec.length();
+			moveSize *= ElapsedTime * 5.0f;
+			//角度
+			float cntlAngle = atan2(moveZ, moveX);
+			//ベクトル作成
+			m_Speed = Vec3(cos(cntlAngle), 0, sin(cntlAngle));
+			//正規化する
+			m_Speed.normalize();
+			//移動サイズを設定
+			m_Speed *= moveSize;
+
+			m_Speed.y = 0;
+		}
+
+		else
+		{
+			m_Speed = Vec3(0.0f);
+
+		}
+
+	}
+
+	void Player::OnUpdate()
+	{
+		SetSpeed();
+
+		auto transComp = GetComponent<Transform>();
+		auto pos = transComp->GetPosition();
+		pos += m_Speed;
+		transComp->SetPosition(pos); // 更新した座標をTransformに設定
+
+	}
 
 }
 //end basecross
--- GameSources/Player.h
+++ GameSources/Player.h
@@ -7,8 +7,19 @@
 #include "stdafx.h"
 
 namespace basecross{
+	class Player : public GameObject {
+		Vec3 m_Speed;
+	public:
+		Player(const std::shared_ptr<Stage>& stage) 
+			: GameObject(stage)
+		{
 
+		}
 
+		void OnCreate() override;
+		void OnUpdate() override;
+		void SetSpeed();
+	};
 }
 //end basecross
 
