63bd0039a6cca7a88970292c84869e056cd59052
Kanazawa Shion <iroha1521@gmail.com>
2021-03-18 13:04:10 +0900

    MainCameraクラスを追加した

---
+++ GameSources/MainCamera.cpp
@@ -0,0 +1,11 @@
+/*!
+@file MainCamera.cpp
+@brief ƒƒCƒ“ƒJƒƒ‰‚È‚ÇÀ‘Ì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+}
---
+++ GameSources/MainCamera.h
@@ -0,0 +1,15 @@
+/*!
+@file MainCamera.h
+@brief ƒƒCƒ“ƒJƒƒ‰‚È‚Ç
+*/
+
+#pragma once
+#include "stdafx.h"
+//#include "InputHandler.h"
+
+namespace basecross {
+	class MainCamera :public GameObject
+	{
+
+	};
+}
