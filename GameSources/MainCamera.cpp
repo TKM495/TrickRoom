c05a8de10159eb6b15eacd27a8747ca0155fd013
Kanazawa Shion <iroha1521@gmail.com>
2021-03-19 11:56:00 +0900

    カメラの位置を変更した。

--- GameSources/MainCamera.cpp
+++ GameSources/MainCamera.cpp
@@ -9,7 +9,7 @@
 namespace basecross {
 
 	MainCamera::MainCamera()
-		:m_offset(0.0f, 5.0f, -5.0f)
+		:m_offset(10.0f, 10.0f, -10.0f),m_Angle(10.0f, -90.0f, -10.0f),bSetPers(false),SetWidth(20.0f),SetHeight(12.5f)
 	{
 
 	}
@@ -20,6 +20,42 @@ namespace basecross {
 	}
 
 	void MainCamera::OnUpdate()
+	{
+		//auto& app = App::GetApp(); // アプリケーションオブジェクトを取得
+		//auto scene = app->GetScene<Scene>(); // アプリケーションオブジェクトからシーンを取得
+		//auto stage = scene->GetActiveStage(); // シーンからステージを取得する
+		//auto gameObjects = stage->GetGameObjectVec();
+		//std::shared_ptr<Player> player;
+
+		//for (auto gameObject : gameObjects)
+		//{
+		//	player = dynamic_pointer_cast<Player>(gameObject);
+		//	if (player)
+		//	{
+		//		break;
+		//	}
+		//}
+
+
+		//auto TransComp = player->GetComponent<Transform>();
+		//auto at = TransComp->GetPosition();
+
+		//SetAt(at);
+
+		//SetEye(at + m_offset);
+
+		
+
+		m_InputHandler.PushHandle(GetThis<MainCamera>());
+
+	}
+
+	Vec3 MainCamera::GetAngle()
+	{
+		return m_Angle;
+	}
+
+	void MainCamera::OnPushB()
 	{
 		auto& app = App::GetApp(); // アプリケーションオブジェクトを取得
 		auto scene = app->GetScene<Scene>(); // アプリケーションオブジェクトからシーンを取得
@@ -36,7 +72,6 @@ namespace basecross {
 			}
 		}
 
-
 		auto TransComp = player->GetComponent<Transform>();
 		auto at = TransComp->GetPosition();
 
@@ -44,6 +79,30 @@ namespace basecross {
 
 		SetEye(at + m_offset);
 	}
-
 	
+	void MainCamera::OnPushA()
+	{
+		auto& app = App::GetApp(); // アプリケーションオブジェクトを取得
+		auto scene = app->GetScene<Scene>(); // アプリケーションオブジェクトからシーンを取得
+		auto stage = scene->GetActiveStage(); // シーンからステージを取得する
+		auto gameObjects = stage->GetGameObjectVec();
+		std::shared_ptr<Player> player;
+
+		for (auto gameObject : gameObjects)
+		{
+			player = dynamic_pointer_cast<Player>(gameObject);
+			if (player)
+			{
+				break;
+			}
+		}
+
+		auto TransComp = player->GetComponent<Transform>();
+		auto at = TransComp->GetPosition();
+
+		SetAt(at);
+
+		SetEye(at + m_Angle);
+	}
+
 }
--- GameSources/MainCamera.h
+++ GameSources/MainCamera.h
@@ -5,18 +5,32 @@
 
 #pragma once
 #include "stdafx.h"
+#include "InputHandler.h"
 
 namespace basecross {
 	class MainCamera :public Camera
 	{
+		InputHandler<MainCamera> m_InputHandler;
 
 		Vec3 m_offset;
+		Vec3 m_Angle;
+		bool bSetPers;
+		float SetWidth;
+		float SetHeight;
+		Vec3 GetAngle();
 
 	public:
 		MainCamera();
 
 		void OnCreate() override;
 		void OnUpdate() override;
+
+		void OnPushA();
+		void OnPushB();
+		void OnPushX() {}
+		void OnPushY() {}
+		void OnPushStart(){}
+		void OnPushBack() {}
 
 	};
 }
