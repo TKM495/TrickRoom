cf2b85a00104c4c19e86e8ff7899fc1ade19697d
Naoya Shida <zeal20300078@c-league.jp>
2021-03-24 14:00:33 +0900

    UI_HPを追加した

---
+++ GameSources/HP.cpp
@@ -0,0 +1,60 @@
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+	void HP::OnCreate()
+	{
+		Col4 color(1.0f, 1.0f, 1.0f, 0.5f);
+		std::vector<VertexPositionColorTexture> vertices = {
+			{Vec3(0.0f,    0.0f, 0.0f), color, Vec2(0.0f,0.0f)},
+			{Vec3(400.0f,    0.0f, 0.0f), color, Vec2(400.0f / 512.0f,0.0f)},
+			{Vec3(0.0f, -100.0f, 0.0f), color, Vec2(0.0f, 100.f / 512.0f)},
+			{Vec3(400.0f, -100.0f, 0.0f), color, Vec2(400.0f / 512.0f, 100.f / 512.0f)}
+		};
+		std::vector<uint16_t> indices = {
+			0, 1, 2,
+			2, 1, 3
+		};
+		auto drawComp = AddComponent<PCTSpriteDraw>(vertices, indices);
+		drawComp->SetTextureResource(L"heart");
+
+		SetAlphaActive(true);
+
+		auto transComp = AddComponent<Transform>();
+		Vec3 pos(-640.0f, +350.0f, 0.0f);
+		transComp->SetPosition(pos);
+
+		// 数字部分の初期化
+		numbers.resize(1);
+		Vec3 offset(150.0f, 0, 0);
+		for (auto& number : numbers)
+		{
+			number = ObjectFactory::Create<Numbers>(GetStage(), 0); // 新しいオブジェクトを生成する。ただし、ステージには追加しない。
+			auto numberTrans = number->GetComponent<Transform>();
+			numberTrans->SetPosition(pos + offset); // SCOREラベルの隣に並ぶ数字
+			offset += Vec3(50.0f, 0, 0); // 数字の幅の文
+		}
+	}
+
+	void HP::OnUpdate()
+	{
+		//int place = static_cast<int>(pow(10, numbers.size() - 1));
+		//for (auto& number : numbers)
+		//{
+		//	int value = hp / place % 10;
+		//	place /= 10;
+
+		//	number->SetValue(value); // 数字を更新
+		//}
+
+	}
+
+	void HP::OnDraw()
+	{
+		GameObject::OnDraw();// (heartの文字が表示される）
+		for (auto& number : numbers) // 各桁の数字を描画する
+		{
+			number->OnDraw();
+		}
+	}
+}
