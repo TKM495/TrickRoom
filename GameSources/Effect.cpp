b12024ec6bf6e55c0da83b2410ec01cf52e8dec6
Kanazawa Shion <iroha1521@gmail.com>
2021-04-06 14:11:00 +0900

    敵とクリスタルでエフェクトの表現(色・数)を調節して変えた

--- GameSources/Effect.cpp
+++ GameSources/Effect.cpp
@@ -9,7 +9,7 @@
 namespace basecross {
 	void Effect::InsertEffect(const Vec3& Pos)
 	{
-		auto ParticlePtr = InsertParticle(5);
+		auto ParticlePtr = InsertParticle(4);
 		ParticlePtr->SetEmitterPos(Pos);
 		ParticlePtr->SetTextureResource(L"Spark");
 		ParticlePtr->SetMaxTime(0.2f);
@@ -19,9 +19,9 @@ namespace basecross {
 			rParticleSprite.m_LocalPos.z = Util::RandZeroToOne() * 0.1f - 0.05f;
 			//各パーティクルの移動速度を指定
 			rParticleSprite.m_Velocity = Vec3(
-				rParticleSprite.m_LocalPos.x * 80.0f,
-				rParticleSprite.m_LocalPos.y * 80.0f,
-				rParticleSprite.m_LocalPos.z * 80.0f
+				rParticleSprite.m_LocalPos.x * 100.0f,
+				rParticleSprite.m_LocalPos.y * 50.0f,
+				rParticleSprite.m_LocalPos.z * 100.0f
 			);
 			//色の指定
 			rParticleSprite.m_Color = Col4(1.0f, 0.0f, 0.0f, 1.0f);
@@ -30,19 +30,19 @@ namespace basecross {
 
 	void Effect::CrystalEffect(const Vec3& Pos)
 	{
-		auto ParticlePtr = InsertParticle(10);
+		auto ParticlePtr = InsertParticle(5);
 		ParticlePtr->SetEmitterPos(Pos);
 		ParticlePtr->SetTextureResource(L"C_Spark");
-		ParticlePtr->SetMaxTime(0.1f);
+		ParticlePtr->SetMaxTime(0.4f);
 		for (auto& rParticleSprite : ParticlePtr->GetParticleSpriteVec()) {
 			rParticleSprite.m_LocalPos.x = Util::RandZeroToOne() * 0.1f - 0.05f;
 			rParticleSprite.m_LocalPos.y = Util::RandZeroToOne() * 0.1f;
 			rParticleSprite.m_LocalPos.z = Util::RandZeroToOne() * 0.1f - 0.05f;
 			//各パーティクルの移動速度を指定
 			rParticleSprite.m_Velocity = Vec3(
-				rParticleSprite.m_LocalPos.x * 30.0f,
-				rParticleSprite.m_LocalPos.y * 30.0f,
-				rParticleSprite.m_LocalPos.z * 30.0f
+				rParticleSprite.m_LocalPos.x * 70.0f,
+				rParticleSprite.m_LocalPos.y * 70.0f,
+				rParticleSprite.m_LocalPos.z * 70.0f
 			);
 			//色の指定
 			rParticleSprite.m_Color = Col4(1.0f, 1.0f, 0.0f, 1.0f);
