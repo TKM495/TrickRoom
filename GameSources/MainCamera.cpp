cfc85ad6f1d3e28391a34fb4eb6237f71c202f1c
Kanazawa Shion <iroha1521@gmail.com>
2021-04-12 16:46:12 +0900

    カメラの位置を調整した

--- GameSources/MainCamera.cpp
+++ GameSources/MainCamera.cpp
@@ -10,7 +10,7 @@ namespace basecross {
 
 	MainCamera::MainCamera()
 		:m_offset(10.0f, 10.0f, -10.0f), m_Angle(-10.0f, 10.0f, -10.0f), bSetPers(false), SetWidth(20.0f), SetHeight(12.5f), m_CameraState(state::Right),
-		bLeapFlg(false), m_LeapTime(0), m_LeapSpeed(1), m_LeapOffset(m_offset)
+		bLeapFlg(false), m_LeapTime(0), m_LeapSpeed(1), m_LeapOffset(m_offset),m_CameraOffset(-5.0,0,0)
 	{
 		isFirst = true;
 	}
@@ -43,7 +43,7 @@ namespace basecross {
 
 		//カメラ処理
 		auto TransComp = player->GetComponent<Transform>();
-		auto at = TransComp->GetPosition();
+		auto at = TransComp->GetPosition() + m_CameraOffset;
 
 		SetAt(at);
 
